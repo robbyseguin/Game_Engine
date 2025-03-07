4d3941501601bdf64d6c9124cee40e2bda530983
robbyseguin <robbyseguin@gmail.com>
2025-03-07 09:52:52 -0500

    Finished Basic ECS
    Working and cleaned up

--- GameEngine/GameEngine/ComponentManager.h
+++ GameEngine/GameEngine/ComponentManager.h
@@ -9,17 +9,36 @@
 class ComponentManager
 {
 public:
+	template <typename T>
+	void AddComponent(Entity entity, T& component)
+	{
+		entityComponents[typeid(T)][entity] = std::make_shared<std::remove_const_t<T>>(component);
+	}
+
+	template <typename... T>
+	void AddComponents(Entity entity, const T&... components) {
+		(AddComponent(entity, components), ...);
+	}
 
 	template <typename T>
-	void AddComponent(Entity entity, T& component);
+	T* GetComponent(Entity entity)
+	{
+		auto typeIt = entityComponents.find(typeid(T));
+		if (typeIt != entityComponents.end()) {
+			auto entityIt = typeIt->second.find(entity);
+			if (entityIt != typeIt->second.end()) {
+				return static_cast<T*>(entityIt->second.get());
+			}
+		}
+		return nullptr;
+	}
 
 	template <typename T>
-	T* GetComponent(Entity entity);
-
-	template <typename T>
-	void RemoveComponent(Entity entity);
+	void RemoveComponent(Entity entity) {
+		entityComponents[typeid(T)].erase(entity);
+	}
 
 private: 
-	std::unordered_map<Entity, std::unordered_map<std::type_index, std::shared_ptr<void>>> entityComponents;
+	std::unordered_map<std::type_index, std::unordered_map<Entity, std::shared_ptr<void>>> entityComponents;
 };
 
--- GameEngine/GameEngine/GameEngine.cpp
+++ GameEngine/GameEngine/GameEngine.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include "EntityManager.h"
+#include "ComponentManager.h"
+#include "Components.h"
+
 
 int main()
 {
-    
+	EntityManager entityManager;
+	ComponentManager componentManager;
+
+	Entity player = entityManager.CreateEntity();
+
+	Position pos = { 0.0f, 0.0f };
+	Velocity vel = { 1.0f, 1.5f };
+	Health health = { 100 };
+
+	componentManager.AddComponents(player, pos, vel, health);
+
+	Position* posPtr = componentManager.GetComponent<Position>(player);
+	Velocity* velPtr = componentManager.GetComponent<Velocity>(player);
+	Health* healthPtr = componentManager.GetComponent<Health>(player);
+	
+	std::cout << "Position: " << posPtr->x << ", " << posPtr->y << std::endl;
+	std::cout << "Velocity: " << velPtr->x << ", " << velPtr->y << std::endl;
+	std::cout << "Health: " << healthPtr->value << std::endl;
+
+	entityManager.DestroyEntity(player);
+
+	return 0;
 }
